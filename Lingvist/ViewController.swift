4aab6aaf76394fe8f62e45a72b47cb66374217bb
zhaoyi <zhaoyi@zhaoyideMacBook-Pro.local>
2017-12-16 03:35:56 +0100

    Initial Commit

---
+++ Lingvist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lingvist
+//
+//  Created by zhaoyi on 2017/12/16.
+//  Copyright © 2017年 if26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LingvistTests/LingvistTests.swift
@@ -0,0 +1,36 @@
+//
+//  LingvistTests.swift
+//  LingvistTests
+//
+//  Created by zhaoyi on 2017/12/16.
+//  Copyright © 2017年 if26. All rights reserved.
+//
+
+import XCTest
+@testable import Lingvist
+
+class LingvistTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
